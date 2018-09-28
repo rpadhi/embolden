b23de0ba1dd2db1bbaef548e3d6edf6eba308dd7
Roshni <rpadhi@ucls.uchicago.edu>
2018-09-27 19:21:05 -0500

    back to normal

--- forms.py
+++
@@ -1,6 +0,0 @@
-from django import forms
-from django.utils.safestring import mark_safe
-
-class CardForm(forms.Form):
-    card_text = forms.CharField(label="", widget=forms.Textarea(attrs={'rows':40, 'cols':75}))
-    
